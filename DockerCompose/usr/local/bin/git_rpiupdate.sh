ad155926188542be19978423fd0bb1884ccc3686
Michael Eichinger <raspidocker@mgeo.de>
2016-09-09 22:44:24 +0200

    Script for deploy GIT-Files on host

---
+++ DockerCompose/usr/local/bin/git_rpiupdate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+REPO_BASE=/var/docker/build
+REPOS="DockerImages DockerTools"
+git_pass=$1
+git_user=raspidocker
+git_url=github.com/${git_user}
+
+for r in ${REPOS}; do
+        REPO_L=${REPO_BASE}/${r}
+        if [ ! -d $REPO_L ]; then
+                echo "Clone Repo $REPO_L"
+                if [ $git_pass = "" ]; then
+                    git clone https://${git_url}${r}
+                else
+                    git clone https://${git_user}:${git_pass}@${git_url}${r}
+                fi
+        else
+                echo "Update Repo $REPO_L"
+                cd ${REPO_L}
+                git pull
+        fi
+done
