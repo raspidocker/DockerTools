5ff8187c55d273f00418155c3e724e083c753325
Michael Eichinger <raspidocker@mgeo.de>
2016-09-09 23:58:45 +0200

    buildScripts

--- DockerCompose/setup.sh
+++ DockerCompose/setup.sh
@@ -16,3 +16,8 @@ echo "Copy source from local-repository to target"
 copyfile ./etc/init.d/dockercomposer /etc/init.d/dockercomposer
 link /etc/init.d/dockercomposer /usr/local/bin/dockercomposer
 copyfile ./var/docker/compose/$(hostname)-*.yml /var/docker/compose/
+
+for _bin in $(ls ./var/docker/bin/*); do
+    link $_bin /usr/local/bin/$(basename $_bin)
+    chmod a+x /usr/local/bin/$(basename $_bin)
+done
